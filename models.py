7c6390bdba31de37ab1c733509acf2070f1446fc
Derese4803 <derese.esubalew-c@oneacrefund.org>
2025-12-31 11:55:30 +0300

    Update models.py
    Signed-off-by: Derese4803 <derese.esubalew-c@oneacrefund.org>

--- models.py
+++ models.py
@@ -1,48 +1,29 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from database import Base, engine
+import datetime
 
-class Woreda(Base):
-    __tablename__ = "woredas"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True)
-    
-    # Relationship to link Kebeles to this Woreda
-    kebeles = relationship("Kebele", back_populates="parent_woreda", cascade="all, delete-orphan")
+Base = declarative_base()
 
-class Kebele(Base):
-    __tablename__ = "kebeles"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    woreda_id = Column(Integer, ForeignKey("woredas.id"))
-    
-    # Relationship back to the Woreda
-    parent_woreda = relationship("Woreda", back_populates="kebeles")
-
-class Farmer(Base):
-    __tablename__ = "farmers"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    phone = Column(String)
-    woreda = Column(String)
-    kebele = Column(String)
-    
-    # This name must match exactly with what is in app.py
-    officer_name = Column(String) 
-    
-    audio_url = Column(String)
-    
-    # --- 2026 Seedling Varieties ---
-    gesho = Column(Integer, default=0)
-    giravila = Column(Integer, default=0)
-    diceres = Column(Integer, default=0)
-    wanza = Column(Integer, default=0)
-    papaya = Column(Integer, default=0)
-    moringa = Column(Integer, default=0)
-    lemon = Column(Integer, default=0)
-    arzelibanos = Column(Integer, default=0)
-    guava = Column(Integer, default=0)
-
-# Helper to initialize the database file
-def create_tables():
-    Base.metadata.create_all(bind=engine)
+class BackCheck(Base):
+    __tablename__ = 'back_checks'
+    id = Column(Integer, primary_key=True)
+    checker_name = Column(String)
+    fenced = Column(String)
+    # Guava
+    guava_beds = Column(Integer)
+    guava_length = Column(Float)
+    guava_sockets = Column(Integer)
+    # Lemon
+    lemon_beds = Column(Integer)
+    lemon_length = Column(Float)
+    lemon_sockets = Column(Integer)
+    # Gesho
+    gesho_beds = Column(Integer)
+    gesho_length = Column(Float)
+    gesho_sockets = Column(Integer)
+    # Grevillea
+    grevillea_beds = Column(Integer)
+    grevillea_length = Column(Float)
+    grevillea_sockets = Column(Integer)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
