6f346c177e57d575c3ea783a818ba78f33d06fb0
Derese4803 <derese.esubalew-c@oneacrefund.org>
2025-12-31 14:26:17 +0300

    Update models.py
    Signed-off-by: Derese4803 <derese.esubalew-c@oneacrefund.org>

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class BackCheck(Base):
     __tablename__ = 'oaf_back_checks'
     id = Column(Integer, primary_key=True)
     
-    # Location and Identifiers
+    # Location & Personnel
     woreda = Column(String)           # ወረዳ
     cluster = Column(String)         # ክላስተር
     kebele = Column(String)           # ቀበሌ
@@ -18,25 +18,25 @@ class BackCheck(Base):
     checker_phone = Column(String)    # ስልክ ቁጥር
     fenced = Column(String)           # አጥር አለው?
 
-    # Guava Group (ዘይቶን)
+    # Guava (ዘይቶን)
     guava_beds = Column(Integer)
     guava_length = Column(Float)
     guava_sockets = Column(Integer)
     total_guava_sockets = Column(Integer)
 
-    # Gesho Group (ጌሾ)
+    # Gesho (ጌሾ)
     gesho_beds = Column(Integer)
     gesho_length = Column(Float)
     gesho_sockets = Column(Integer)
     total_gesho_sockets = Column(Integer)
 
-    # Lemon Group (ሎሚ)
+    # Lemon (ሎሚ)
     lemon_beds = Column(Integer)
     lemon_length = Column(Float)
     lemon_sockets = Column(Integer)
     total_lemon_sockets = Column(Integer)
 
-    # Grevillea Group (ግራቪሊያ)
+    # Grevillea (ግራቪሊያ)
     grevillea_beds = Column(Integer)
     grevillea_length = Column(Float)
     grevillea_sockets = Column(Integer)
