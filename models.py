7b61ae915c0114789ab90c6a4825429ded6c7ccc
Derese4803 <derese.esubalew-c@oneacrefund.org>
2025-12-31 13:49:36 +0300

    Update models.py
    Signed-off-by: Derese4803 <derese.esubalew-c@oneacrefund.org>

--- models.py
+++ models.py
@@ -5,36 +5,39 @@ import datetime
 Base = declarative_base()
 
 class BackCheck(Base):
-    __tablename__ = 'back_checks'
+    __tablename__ = 'oaf_back_checks'
     id = Column(Integer, primary_key=True)
     
     # Location and Identifiers
     woreda = Column(String)
     kebele = Column(String)
-    checker_fa_name = Column(String)  # Name of Back checker (FAs)
-    checker_cbe_name = Column(String) # Back checker (CBE)
-    checker_phone = Column(String)    # Back checker phone #
-    fenced = Column(String)           # Nursery Fenced (Yes/No)
-    
-    # Guava Metrics (Expect 13)
+    checker_fa_name = Column(String)
+    checker_cbe_name = Column(String) 
+    checker_phone = Column(String)    
+    fenced = Column(String)           
+
+    # Guava Group
     guava_beds = Column(Integer)
     guava_length = Column(Float)
     guava_sockets = Column(Integer)
-    
-    # Lemon Metrics (Expect 13)
-    lemon_beds = Column(Integer)
-    lemon_length = Column(Float)
-    lemon_sockets = Column(Integer)
-    total_lemon_sockets = Column(Integer) # Auto-calculated
-    
-    # Gesho Metrics (Expect 16)
+    total_guava_sockets = Column(Integer) # Automatic
+
+    # Gesho Group
     gesho_beds = Column(Integer)
     gesho_length = Column(Float)
     gesho_sockets = Column(Integer)
-    
-    # Grevillea Metrics (Expect 16)
+    total_gesho_sockets = Column(Integer) # Automatic
+
+    # Lemon Group
+    lemon_beds = Column(Integer)
+    lemon_length = Column(Float)
+    lemon_sockets = Column(Integer)
+    total_lemon_sockets = Column(Integer) # Automatic
+
+    # Grevillea Group
     grevillea_beds = Column(Integer)
     grevillea_length = Column(Float)
     grevillea_sockets = Column(Integer)
+    total_grevillea_sockets = Column(Integer) # Automatic
     
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)
