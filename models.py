3e58789b6919b9e03763c23574281554a83ee9ed
Derese4803 <derese.esubalew-c@oneacrefund.org>
2026-01-01 11:08:56 +0300

    Update models.py
    Signed-off-by: Derese4803 <derese.esubalew-c@oneacrefund.org>

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, Text
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
 
@@ -7,25 +7,16 @@ Base = declarative_base()
 class BackCheck(Base):
     __tablename__ = 'oaf_back_checks'
     id = Column(Integer, primary_key=True)
+    woreda = Column(String); cluster = Column(String); kebele = Column(String); tno_name = Column(String)
+    checker_fa_name = Column(String); cbe_acc = Column(String); checker_phone = Column(String); fenced = Column(String)
     
-    # Personnel & Location
-    woreda = Column(String)
-    cluster = Column(String)
-    kebele = Column(String)
-    tno_name = Column(String)
-    checker_fa_name = Column(String)
-    cbe_acc = Column(String)
-    checker_phone = Column(String)    
-    fenced = Column(String)           
-
-    # Species Data
     guava_beds = Column(Integer); guava_length = Column(Float); guava_sockets = Column(Integer); total_guava_sockets = Column(Integer)
     gesho_beds = Column(Integer); gesho_length = Column(Float); gesho_sockets = Column(Integer); total_gesho_sockets = Column(Integer)
     lemon_beds = Column(Integer); lemon_length = Column(Float); lemon_sockets = Column(Integer); total_lemon_sockets = Column(Integer)
     grevillea_beds = Column(Integer); grevillea_length = Column(Float); grevillea_sockets = Column(Integer); total_grevillea_sockets = Column(Integer) 
     
-    # Remarks / አስተያየቶች
-    auto_remark = Column(String)    # Automatic Over/Under status
-    general_remark = Column(String) # Manual user comments
+    auto_remark = Column(String)
+    general_remark = Column(String)
+    photo = Column(Text)  # Added to store base64 image data
     
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)
